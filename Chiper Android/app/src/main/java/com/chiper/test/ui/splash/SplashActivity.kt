c857f918d5610a3e3106b91a1b1a2cfb92be1451
jlombana <alombana24@hotmail.com>
2022-02-17 14:48:50 -0500

    Splash full screen

--- Chiper Android/app/src/main/java/com/chiper/test/ui/splash/SplashActivity.kt
+++ Chiper Android/app/src/main/java/com/chiper/test/ui/splash/SplashActivity.kt
@@ -1,8 +1,13 @@
 package com.chiper.test.ui.splash
 
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.chiper.test.MainActivity
 import com.chiper.test.R
 import java.util.*
@@ -11,6 +16,16 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+        else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         Timer().schedule(object : TimerTask(){
             override fun run() {
